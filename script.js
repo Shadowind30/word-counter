c1850627167f388e4828e6d566eee019dd9f6c94
Shadowind30 <shadowind30@gmail.com>
2020-03-26 21:22:01 -0400

    Added basic funcionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('sucessful init')
+const main = document.querySelector('#main');
+const counter = document.querySelector('#counter');
+
+let currentLength = main.value.length;
+
+const updateCounter = (wordCount) => {
+  if (currentLength === 0){
+    counter.innerHTML = 'Paste your text in the form above';
+  } else{
+    counter.innerHTML = `Word Count: ${wordCount}`;
+  }
+
+}
+const countWords = () => {
+  let words = [];
+  words = currentLength = main.value.split(' ').length;
+  updateCounter(words);
+};
